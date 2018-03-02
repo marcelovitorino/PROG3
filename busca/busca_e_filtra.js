3ed638a40c28c92482d00102c9f20d57acb6b99a
Marcelo Gabriel Dos Santos Queiroz Vitorino 116211290 <you@example.com>
2018-03-02 17:19:34 -0300

    adicionando novo recurso

--- busca/busca_e_filtra.js
+++ busca/busca_e_filtra.js
@@ -1,5 +1,11 @@
 function busca_e_filtra(url_base) {
-	fetch(url_base)
+	recurso1 = fetch(url_base + "/data")
   .then(response => response.json())
-  .then(data => console.log('data is', data))
+  .then(data => console.log('data is', data));
+  
+  	recurso2 = fetch(url_base + dados.recurso)
+  .then(response => response.json())
+  .then(data => console.log('data is', data));
+  
+  
 }
