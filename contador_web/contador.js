7a665d5f28f4f30eb0cb56dd558dd9a482515f6e
Marcelo Gabriel Dos Santos Queiroz Vitorino 116211290 <you@example.com>
2018-03-02 16:35:48 -0300

    iniciando busca e filtra

--- contador_web/contador.js
+++ contador_web/contador.js
@@ -1,13 +1,16 @@
 /* Marcelo Vitorino - Prog3 - UFCG
 */
 var identificador = null; 
+var state;
  
 function start(tempo){
     if(!identificador){
       identificador = setInterval(update, tempo);
+      state = iniciado;
       }
     }
   function stop() {
       clearInterval(identificador);
       identificador = null;
+      state = parado;
 }
